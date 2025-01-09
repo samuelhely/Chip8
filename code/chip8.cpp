ee96c25c2336f58c75e2482b1e25774c488ca093
Samuel <samuel_hely@hotmail.com>
2025-01-09 18:27:55 -0300

    Added main variables components

--- code/chip8.cpp
+++ code/chip8.cpp
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <stdint.h>
+
+#define Kilobytes(Value)((Value) * 1024)
+#define Megabytes(Value)(Kilobytes(Value) * 1024)
+
+uint8_t Registers[16];
+uint16_t OpCode;
+uint8_t Memory[Kilobytes(4)];
+uint16_t IndexRegister;
+uint16_t ProgramCounter;
+uint16_t Stack[16];
+uint8_t StackPointer;
+uint8_t DelayTimer;
+uint8_t SoundTimer;
+uint8_t Keypad[16];
+uint8_t Display[64*32];
 
 int main(int ArgCount, char **ArgValues)
 {
