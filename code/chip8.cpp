c97e4e450cb7cb48b0ae5c6b69012853589bd0e4
Samuel <samuel_hely@hotmail.com>
2025-01-25 18:32:59 -0300

    Basic fetch and decode loop

--- code/chip8.cpp
+++ code/chip8.cpp
@@ -1,5 +1,8 @@
+#include "raylib.h"
+
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef uint8_t uint8;
 typedef uint16_t uint16;
@@ -47,7 +50,7 @@ uint8_t Font[] = {
 
 void InitializeEmulator() {
     IndexRegister = 0;
-    ProgramCounter = 0;
+    ProgramCounter = 0x200;
     StackPointer = 0;
     OpCode = 0;
     DelayTimer = 0;
@@ -273,20 +276,161 @@ void Opcode_9XY0()
     }
 }
 
+void Opcode_ANNN()
+{
+    uint16 Value = OpCode & 0x0FFF;
+    IndexRegister = Value;
+}
+
+void Opcode_BNNN()
+{
+    uint16 Value = OpCode & 0x0FFF;
+    ProgramCounter = Value + Registers[0];
+}
+
+void Opcode_CXKK()
+{
+    uint8 VX = OpCode & 0x0F00;
+    uint8 Rand = (uint8)rand();
+    uint8 Value = OpCode & 0x00FF;
+
+    Registers[VX] = Rand & Value;
+}
+
+void Opcode_DXYN()
+{
+    uint8 VX = OpCode & 0x0F00;
+    uint8 VY = OpCode & 0x00F0;
+    uint8 Value = OpCode & 0x000F;
+
+    // NOTE(samuel): The interpreter reads n bytes from memory, starting at the address stored in I. 
+    // These bytes are then displayed as sprites on screen at coordinates (Vx, Vy). 
+    // Sprites are XORed onto the existing screen. 
+    // If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0. 
+    // If the sprite is positioned so part of it is outside the coordinates of the display, 
+    // it wraps around to the opposite side of the screen. 
+}
+
 int main(int ArgCount, char **ArgValues)
 {
     InitializeEmulator();
 
+    uint16 Offset = 0x200;
     if(ArgCount > 1)
     {
-        uint32 Offset = 0x200;
         char* Filename = ArgValues[1];
         FILE *File = fopen(Filename, "rb");
         if(File)
         {
-            fread(Memory + Offset, 1, ArrayCount(Memory) - Offset, File);
+            fread(Memory + ProgramCounter, 1, ArrayCount(Memory) - ProgramCounter, File);
             fclose(File);
         }
+    }
+
+    uint16 Instruction;
+
+    InitWindow(640, 480, "Chip8");
+    while(!WindowShouldClose())
+    {
+        // Fetching
+        Instruction = (uint16)(((uint16)Memory[ProgramCounter] << 8) | Memory[ProgramCounter + 1]);
+        ProgramCounter += 2;
+        
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+        uint8 OpCode = (Instruction & 0xF000) >> 8;
+        switch(OpCode)
+        {
+            case 0x0 :
+            {
+                if(Instruction == 0x00E0)
+                {
+                    TraceLog(LOG_WARNING, "Clear the screen");
+                }
+                else if(Instruction == 0x00EE)
+                {
+                    TraceLog(LOG_WARNING, "Clear the screen");
+                }
+            } break;
+
+            case 0x10 :
+            {
+                TraceLog(LOG_WARNING, "Jump to address");
+            } break;
+
+            case 0x20 :
+            {
+                TraceLog(LOG_WARNING, "Call at address");
+            } break;
+
+            case 0x30 :
+            {
+                TraceLog(LOG_WARNING, "Skip");
+            } break;
+
+            case 0x40 :
+            {
+                TraceLog(LOG_WARNING, "Instruction 4");
+            } break;
+
+            case 0x50 :
+            {
+                TraceLog(LOG_WARNING, "Instruction 5");
+            } break;
+
+            case 0x60 :
+            {
+                TraceLog(LOG_WARNING, "Instruction 6");
+            } break;
+
+            case 0x70 :
+            {
+                TraceLog(LOG_WARNING, "Instruction 7");
+            } break;
+
+            case 0x80 :
+            {
+                TraceLog(LOG_WARNING, "Instruction 8");
+            } break;
+
+            case 0x90 :
+            {
+                TraceLog(LOG_WARNING, "Instruction 9");
+            } break;
+
+            case 0xA0 :
+            {
+                TraceLog(LOG_WARNING, "Instruction A");
+            } break;
+
+            case 0xB0 :
+            {
+                TraceLog(LOG_WARNING, "Instruction B");
+            } break;
+
+            case 0xC0 :
+            {
+                TraceLog(LOG_WARNING, "Instruction C");
+            } break;
+
+            case 0xD0 :
+            {
+                TraceLog(LOG_WARNING, "Instruction D");
+            } break;
+
+            case 0xE0 :
+            {
+                TraceLog(LOG_WARNING, "Instruction E");
+            } break;
+
+            case 0xF0 :
+            {
+                TraceLog(LOG_WARNING, "Instruction F");
+            } break;
+        }
+
+        EndDrawing();
     }
 
     return 0;
